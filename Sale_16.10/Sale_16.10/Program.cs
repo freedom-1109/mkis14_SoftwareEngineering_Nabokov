fd02b655d8dc1cb6a231180f0a2d91e9e4bdf562
Nabokov Nikolai <66208423+freedom-1109@users.noreply.github.com>
2021-10-19 15:10:05 +0300

    вычисление стоимости покупки

---
+++ Sale_16.10/Sale_16.10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Sale_16._10
+{
+    internal class Program
+    {
+        /*
+            Задание. Написать программу вычисления стоимости покупки с учетом скидки. 
+            Скидка в 3% предоставляется в том случае, 
+            если сумма покупки больше 500 руб., 
+            в 5% - если сумма больше 1000 руб.
+        */
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите стоимость покупки: ");
+            /* стоимость покупок */
+            var price = ReadDouble();
+            
+            /* расчет и вывод итоговой цены */
+            if (price >= 500 && price < 1000)
+                Console.WriteLine($"К оплате: {price * 0.97}");
+            else if (price >= 1000)
+                Console.WriteLine($"К оплате: {price * 0.95}");
+            else
+                Console.WriteLine($"К оплате: {price}");
+        }
+        
+        /* заставляет ввести число */
+        private static double ReadDouble()
+        {
+            double result;
+            while (!double.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Это не число, попробуйте еще раз.");
+            }
+
+            return result;
+        }
+    }
+}
