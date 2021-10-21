cff93d3bd4f71467edf34080068707b9713fc42b
Nabokov Nikolai <66208423+freedom-1109@users.noreply.github.com>
2021-10-21 16:34:41 +0300

    comment

--- 21_10_21/triangle/triangle/Program.cs
+++ 21_10_21/triangle/triangle/Program.cs
@@ -4,12 +4,15 @@ namespace triangle
 {
     internal class Program
     {
+        /*
+         * Вывести на экран, консольного приложения, прямоугольный треугольник из звездочек “*”, с длиной катета m
+         */
         public static void Main(string[] args)
         {
             var m = Convert.ToInt32(Console.ReadLine());
             for (var i = 1; i <= m; i++)
             {
-                for (int j = 0; j < i; j++)
+                for (var j = 0; j < i; j++)
                 {
                     Console.Write("* ");
                 }
