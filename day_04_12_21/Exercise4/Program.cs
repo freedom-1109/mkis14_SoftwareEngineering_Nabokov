0fa4c9871a993f32087ca5d4413a2f5c82301162
Nabokov Nikolai <n.nabokov1109@gmail.com>
2021-12-04 13:26:18 +0300

    1

---
+++ day_04_12_21/Exercise4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            var M = new int[100];
+            for (int i = 0; i < M.Length; i++)
+                M[i] = rnd.Next(-1000, 1000);
+
+            Console.WriteLine(M.Count(i => 0 < i && i < 125));
+        }
+    }
+}
